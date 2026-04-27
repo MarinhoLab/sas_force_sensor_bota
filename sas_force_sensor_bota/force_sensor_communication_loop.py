6bf8f5b912b92b495f7639445a5154b79c2c5bca
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2026-04-27 11:38:24 +0100

    [config] Making configuration file path configurable with parameter 'configuration_file_path'

--- sas_force_sensor_bota/force_sensor_communication_loop.py
+++ sas_force_sensor_bota/force_sensor_communication_loop.py
@@ -10,6 +10,7 @@ You should have received a copy of the GNU General Public License along with thi
 see <https://www.gnu.org/licenses/>.
 """
 import rclpy
+from rclpy.node import Node
 from ament_index_python.packages import get_package_share_directory
 
 import multiprocessing as mp
@@ -26,12 +27,15 @@ def communication_loop(run):
     rclpy.init()
 
     this_package_share_directory = get_package_share_directory('sas_force_sensor_bota')
-
-    # TODO make this configurable
-    config_path = str(this_package_share_directory / pathlib.Path("config") / pathlib.Path(
+    default_configuration_file_path = str(this_package_share_directory / pathlib.Path("config") / pathlib.Path(
         "../config/bota_binary_gen0.json"))
 
-    with ForceSensorBota(config_path) as fsb, mm.SharedMemoryManager() as smm:
+    # TODO: Do we need a node just for this?
+    rospy_node = Node('sas_force_sensor_bota_configuration_subnode')
+    rospy_node.declare_parameter('configuration_file_path', default_configuration_file_path)
+    configuration_file_path = rospy_node.get_parameter('configuration_file_path').get_parameter_value().string_value
+
+    with ForceSensorBota(configuration_file_path) as fsb, mm.SharedMemoryManager() as smm:
 
         lock = mp.Lock()
 
